9e0664a405714b2bfd7ead24b871c6a8303f025b
Takos4Lunch <takeshie01@gmail.com>
2023-07-03 12:30:52 -0400

    3rd commit, progress on db connection

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+/**
+ * RECORDATORIO: cambiar estas variables a variables de ambiente
+ */
+
+const sequelize = new Sequelize('neo_bank','rafael','123456', {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+connect();
+
+
+module.exports = {sequelize};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express(); //App init
+const sequelize = require('./db/connection')
 
 const port = 3000;
 
