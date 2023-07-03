cf5ae531b5fe97a509780e8e89440485106f62d9
Takos4Lunch <takeshie01@gmail.com>
2023-07-03 14:49:41 -0400

    user model created

---
+++ db/models/user.model.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../connection');
+
+class User extends Model{}
+
+User.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },{
+        sequelize,
+        modelName: 'User'
+    }
+)
+
+module.exports = {User}
